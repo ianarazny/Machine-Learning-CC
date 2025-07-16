1b28395da83acb54217bd9b268a4c8b82044cbfd
Ian Arazny <ian.arazny.c@gmail.com>
2025-07-15 21:20:58 -0300

    Data-exploration updates

--- ml/scripts/data_loader.py
+++ ml/scripts/data_loader.py
@@ -1,9 +1,13 @@
 import pandas as pd
-import glob
 import os
 
-def load_measurements(tcp_variant):
-    path = f'../data/{tcp_variant}/'
-    files = glob.glob(os.path.join(path, '*.csv'))
-    dfs = [pd.read_csv(file) for file in files]
+def load_measurements(tcp_variant, base_path='ml/data'):
+    folder = os.path.join(base_path, tcp_variant)
+    all_files = [f for f in os.listdir(folder) if f.endswith('.csv')]
+
+    dfs = []
+    for file in all_files:
+        df = pd.read_csv(os.path.join(folder, file), sep=';')  
+        dfs.append(df)
+
     return pd.concat(dfs, ignore_index=True)
